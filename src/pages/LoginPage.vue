1d8c3a99175cdd7c3cdd04b7cda7954907573816
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-04-21 21:25:00 +0900

    仮実装

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,3 +1,25 @@
 <template>
-    LoginPage
+    <h1>ログイン</h1>
+    <form @submit.prevent="login">
+        <div>
+            <label for="id">ID</label>
+            <input type="id" id="id" v-model="id">
+        </div>
+        <div>
+            <label for="password">パスワード</label>
+            <input type="password" id="password" v-model="password">
+        </div>
+        <button type="submit">ログイン</button>
+    </form>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const id = ref('');
+const password = ref('');
+
+const login = () => {
+    console.log(id.value, password.value)
+}
+</script>
